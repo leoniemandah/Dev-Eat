91050e6c067f7f8aa5b4bc0f48005bd39b402c43
johannadznd <johanna.dezarnaud@ynov.com>
2021-01-09 19:42:30 +0100

    suppr erreur

--- devEat/src/Controller/PanierController.php
+++ devEat/src/Controller/PanierController.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
-
+use Mailgun\Mailgun;
 /**
 * @Route("/user")
 **/
@@ -121,14 +121,11 @@ class PanierController extends AbstractController
             $entityManager->persist($orderMeal);
         }
 
-        
+
           
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->persist($order);
         $entityManager->flush();
-    
-
-
 
         $mgClient = Mailgun::create('');
         $domain = "";
