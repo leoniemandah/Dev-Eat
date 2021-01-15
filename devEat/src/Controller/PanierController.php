4d0163208c532fbaed830a77dd05fcfee29ac8cc
johannadznd <johanna.dezarnaud@ynov.com>
2021-01-15 08:41:29 +0100

    mail bug

--- devEat/src/Controller/PanierController.php
+++ devEat/src/Controller/PanierController.php
@@ -15,7 +15,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Mailgun\Mailgun;
 /**
 * @Route("/user")
 **/
